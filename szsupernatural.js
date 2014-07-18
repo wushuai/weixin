5a28151cbf472b5d734387ee933e29a03bc8d4c4
wushuai <wushuai210@gmail.com>
2014-07-18 17:29:22 +0800

    first commit

---
+++ szsupernatural.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var querystring = require('querystring');
+var url = require('url');
+var crypto = require('crypto');
+http.createServer(function(request, response){
+	console.log('connected!');
+
+	var postData = '';
+	var responseData = '';
+	if(request.method == 'GET'){
+		var objUrl = url.parse(request.url);
+		var objGetParmas = querystring.parse(objUrl.query);
+		
+		var signature = objGetParmas.signature;
+		var timestamp = objGetParmas.timestamp;
+		var nonce = objGetParmas.nonce;
+		var echostr = objGetParmas.echostr;
+
+		console.log(signature);
+		console.log(timestamp);
+		console.log(nonce);
+		console.log(echostr);
+
+		var token = 'wushuaitoken';
+		var array = [token, timestamp, nonce];
+		array.sort();
+		
+		var sha1 = crypto.createHash('sha1');
+		console.log(array.join(''));
+		var tempstr = sha1.update(array.join('')).digest().toString('base64')
+
+		console.log(tempstr);
+
+	}else if(request.method == 'POST'){
+		request.setEncoding('utf8');
+		request.addListener('data', function(postDataChunk){
+			postData += postDataChunk;
+		});
+		request.addListener('end', function(){
+			var objPostData = querystring.parse(postData);
+			console.log(objPostData.name);
+		})
+	}
+	response.writeHead(200, {'Content-Type':'text/plain'});
+	response.end('Hello node');	
+}).listen(80, '121.14.98.48');
+console.log('Server running...');
